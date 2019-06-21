f350608734af6c0e90119695eda82087b4e0bd1f
Juan Henao <juanhenao10@gmail.com>
2019-06-21 14:32:05 +0200

    Add passport token auth to project

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,13 @@ use App\Http\Resources\Collection as CollectionResource;
 |
 */
 
-/*Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::post('login', 'AuthController@login');
+
+Route::post('register', 'AuthController@register');
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-});*/
+});
 
 
 Route::resource('collections', 'CollectionController')->only([
